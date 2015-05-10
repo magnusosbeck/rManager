2109257b9887db865e07660cfb75ea55d562c0c1
Magnus Osbeck <hyllafyra@gmail.com>
2015-05-10 18:07:48 +0200

    some login to dodo's

--- app/modules/partnerModule/controllers/partnerViewController.js
+++ app/modules/partnerModule/controllers/partnerViewController.js
@@ -2,6 +2,16 @@ partnerModule.controller('partnerViewController', ['$scope', 'partnerService', '
 
     var self = this;
 
+    self.parametes = ['initiative',
+        'feelings',
+        'family',
+        'career',
+        'wellRead',
+        'selfInterest',
+        'home',
+        'urbane',
+        'intimacy'];
+
     $scope.user = 'bamam';
     $scope.dodos = {};
 
@@ -11,20 +21,86 @@ partnerModule.controller('partnerViewController', ['$scope', 'partnerService', '
         var currentUser = storageService.load('currentUser');
         console.log(currentUser);
 
-        if(currentUser && currentUser['partner']){
-            //partnerService.read(currentUser['partner']['objectId']).then(function(response){
-            partnerService.read('YV3XDxDmb2').then(function(response){
-                $scope.partner = response;
+        partnerService.read('gsyv8PzpdA').then(function(partnerResponse){
+            $scope.partner1 = partnerResponse;
+            dodoService.get().then(function(dodoResponse){
+
+                var points = 0;
+                for (var i in dodoResponse.results){
+                    dodoResponse.results[i].points = (dodoResponse.results[i].size * 10);
+
+                    for(var p in self.parametes){
+                        var paramName = self.parametes[p];
+
+                        if(typeof dodoResponse.results[i][paramName] == "undefined" || typeof $scope.partner1[paramName] == "undefined"){
+                            continue;
+                        }
+
+                        dodoResponse.results[i].points += dodoResponse.results[i][paramName] * $scope.partner1[paramName];
+                    }
+                    if(!dodoResponse.results[i].points){
+                        dodoResponse.results[i].points = 0;
+                    }
+                    console.log(dodoResponse.results[i].title + ' : ' + dodoResponse.results[i].points)
+                }
+                $scope.dodos1 = dodoResponse.results;
             });
-        }
-        dodoService.get().then(function(response){
-
-            for(var i in response.results){
-                response.results[i].points = 100 + (response.results[i].size * 100);
-            }
-
-            $scope.dodos = response.results;
         });
+
+        partnerService.read('jRwxWrGhV9').then(function(partnerResponse){
+            $scope.partner2 = partnerResponse;
+            dodoService.get().then(function(dodoResponse){
+
+                var points = 0;
+                for (var i in dodoResponse.results){
+                    dodoResponse.results[i].points = (dodoResponse.results[i].size * 10);
+
+                    for(var p in self.parametes){
+                        var paramName = self.parametes[p];
+
+                        if(typeof dodoResponse.results[i][paramName] == "undefined" || typeof $scope.partner2[paramName] == "undefined"){
+                            continue;
+                        }
+
+                        dodoResponse.results[i].points += dodoResponse.results[i][paramName] * $scope.partner2[paramName];
+                    }
+                    if(!dodoResponse.results[i].points){
+                        dodoResponse.results[i].points = 0;
+                    }
+                    console.log(dodoResponse.results[i].title + ' : ' + dodoResponse.results[i].points)
+                }
+                $scope.dodos2 = dodoResponse.results;
+            });
+        });
+
+        //if(currentUser && currentUser['partner']){
+        //    //partnerService.read(currentUser['partner']['objectId']).then(function(response){
+        //    partnerService.read('YV3XDxDmb2').then(function(partnerResponse){
+        //        $scope.partner = partnerResponse;
+        //        dodoService.get().then(function(dodoResponse){
+        //
+        //            var points = 0;
+        //            for (var i in dodoResponse.results){
+        //                dodoResponse.results[i].points = (dodoResponse.results[i].size * 20);
+        //
+        //                for(var p in self.parametes){
+        //                    var paramName = self.parametes[p];
+        //
+        //                    if(typeof dodoResponse.results[i][paramName] == "undefined" || typeof $scope.partner[paramName] == "undefined"){
+        //                        continue;
+        //                    }
+        //
+        //                    dodoResponse.results[i].points += dodoResponse.results[i][paramName] * $scope.partner[paramName];
+        //                }
+        //                if(!dodoResponse.results[i].points){
+        //                    dodoResponse.results[i].points = 0;
+        //                }
+        //                console.log(dodoResponse.results[i].title + ' : ' + dodoResponse.results[i].points)
+        //            }
+        //            $scope.dodos = dodoResponse.results;
+        //        });
+        //    });
+        //}
 
     };
 
